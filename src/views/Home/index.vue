58d871c387339c3a560dbdc9dff0116b8b9bcb47
霍运浩 <372799514@qq.com>
2022-07-08 19:01:48 +0800

    init

---
+++ src/views/Home/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1 class="home-title">Home2~~~</h1>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<style>
+.home-title {
+  color: deeppink;
+  font-size: 3px;
+}
+</style>
